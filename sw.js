e6ef62897ac4edcf12abb2dcd864e7d080a6e8a9
jendryboss-lgtm <jendryboss@gmail.com>
2026-09-19 15:25:14 -0400

    Add styles, service worker, and icon

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "pwsh-ios-v1";
+const ASSETS = ["./" ,"./index.html","./styles.css","./app.js","./manifest.json","./icons/icon.svg"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => {
+    const fetched = fetch(event.request).then((res) => {
+      if (res && res.ok && new URL(event.request.url).origin === self.location.origin) {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+      }
+      return res;
+    }).catch(() => cached);
+    return cached || fetched;
+  }));
+});
